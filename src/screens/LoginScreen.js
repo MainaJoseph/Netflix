e52a7154a32eaa7f0c7d3777a933be47d8a0d610
Joseph Maina <jmaina@kippra.or.ke>
2023-03-14 16:58:52 +0300

    sign up

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,7 +3,6 @@ import FooterScreen from './FooterScreen';
 import './LoginScreen.css';
 import SignupScreen from './SignupScreen';
 
-
 function LoginScreen() {
   const [signIn, setSignIn] = useState(false);
   const [email, setEmail] = useState('');
@@ -13,19 +12,34 @@ function LoginScreen() {
     window.location.reload();
   };
 
-
-  
   const handleGetStartedClick = (e) => {
     e.preventDefault();
     const inputEmail = e.target.form[0].value.trim();
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailInput = document.getElementById('emailInput');
+    
     if (inputEmail === '') {
       setErrorMessage('Please enter your email address');
+      emailInput.classList.add('error');
+      emailInput.classList.remove('valid');
     } else if (!emailRegex.test(inputEmail)) {
       setErrorMessage('Invalid email address');
+      emailInput.classList.add('error');
+      emailInput.classList.remove('valid');
     } else {
       setEmail(inputEmail);
       setSignIn(true);
+      emailInput.classList.add('valid');
+      emailInput.classList.remove('error');
+    }
+  };
+
+  const handleInputChange = () => {
+    const emailInput = document.getElementById('emailInput');
+    if (emailInput.classList.contains('error') || emailInput.classList.contains('valid')) {
+      emailInput.classList.remove('error');
+      emailInput.classList.remove('valid');
+      setErrorMessage('');
     }
   };
 
@@ -42,7 +56,6 @@ function LoginScreen() {
           <button onClick={() => setSignIn(true)} className="loginScreen__button">
             Sign In
           </button>
-
           <div className="loginScreen__gradient" />
         </div>
         <div className="loginScreen__body">
@@ -53,10 +66,14 @@ function LoginScreen() {
               <h1>Unlimited films, TV programmes and more.</h1>
               <h2>Watch anywhere. Cancel anytime.</h2>
               <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-
               <div className="loginScreen__input">
                 <form>
-                  <input type="email" placeholder="Email address" />
+                  <input
+                    id="emailInput"
+                    type="email"
+                    placeholder="Email address"
+                    onChange={handleInputChange}
+                  />
                   <button onClick={handleGetStartedClick} className="loginScreen__getStarted">
                     Get Started &nbsp; >
                   </button>
@@ -68,7 +85,6 @@ function LoginScreen() {
             </>
           )}
         </div>
-
         <div className="loginScreen__border" />
       </div>
       {!signIn && <FooterScreen />}
