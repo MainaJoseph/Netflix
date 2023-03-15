6ceda58ce4ced57191193ab3a7a6458c544c2d2c
Joseph Maina <jmaina@kippra.or.ke>
2023-03-15 08:16:51 +0300

    fix netflix log not rendering

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -51,7 +51,7 @@ function LoginScreen() {
             onClick={handleLogoClick}
             className="loginScreen__logo"
             src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-            alt="Netflix"
+            alt=""
           />
           <button onClick={() => setSignIn(true)} className="loginScreen__button">
             Sign In
